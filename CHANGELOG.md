9ab19c1c652692ec1c6072e0acace347a0f2bbf3
hovanduong <hovanduong8963@gmail.com>
2024-01-21 17:43:34 +0700

    feat: Update Version 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.1
+
+- Initial Readme.
+
 ## 0.0.2
 
 - Change Readme.
--- test/color_from_hex_method_channel_test.dart
+++ test/color_from_hex_method_channel_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 // import 'package:color_from_hex/color_from_hex_method_channel.dart';
 
